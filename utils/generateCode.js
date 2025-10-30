cb93fae300e9db769fd4267bb20be11a1136d727
AtharvSutar05 <atharv.sutar2209@gmail.com>
2025-10-30 11:24:18 +0530

    Improved meeting code generation

--- utils/generateCode.js
+++ utils/generateCode.js
@@ -1,8 +1,15 @@
+const getRandomString = (length, chars) => {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return result;
+};
+
 export const generateMeetingCode = () => {
-    const chars = 'abcdefghijklmnopqrstuvwxyz';
-    const c1 = getRandomString(3, chars);
-    const c2 = getRandomString(3, chars);
-    const c3 = getRandomString(3, chars);
-    const result = `${c1}-${c2}-${c3}`;
-    return result;
+  const chars = 'abcdefghijklmnopqrstuvwxyz';
+  const c1 = getRandomString(3, chars);
+  const c2 = getRandomString(3, chars);
+  const c3 = getRandomString(3, chars);
+  return `${c1}-${c2}-${c3}`;
 };
