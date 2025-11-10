e24b038abb424adac645fda6e7b3b84676a0ff73
AtharvSutar05 <atharv.sutar2209@gmail.com>
2025-11-11 00:57:30 +0530

    version contoller modified

--- controllers/version.controller.js
+++ controllers/version.controller.js
@@ -4,12 +4,15 @@ export const getLatestVersion = async(req, res) => {
     try { 
         res.status(200).json({
             version: "1.0.0",
-            mandatory: false,
+            buildNumber: 1,
+            mandatory: true,
+            url: "https://github.com/AtharvSutar05/snatch-app",
+            sha256: "e9f97c45a7a3fcbce5e4f3b56a0c0fa0d9d1b09c5cbf8a7ef2c2c6adfc2b5e22",
             changelog: "Initial release with core features including meeting creation, joining rooms, and real-time communication."
         });
-    } catch {
+    } catch(err) {
         res.status(500).json({
-            error: "Server Issue",
+            error: `Server Issue: ${err}`,
         });
     }
 };
